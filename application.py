7a8ab028fe1928f42c371580791c381fa05812c9
SOUMYABAN123 <banerjeesoumyasekhar@gmail.com>
2025-12-01 18:06:12 +0100

    web based ap.py, predict_pipeline.py, template folder commited

--- application.py
+++ application.py
@@ -6,14 +6,14 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
-app= application
+
 
 ## Route for home pagae
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata', methods=['GET','POST'])
+@application.route('/predictdata', methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -36,7 +36,7 @@ def predict_datapoint():
     
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
 
 
 
